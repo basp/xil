6d0e41b20c8c1429c0c9039f14419b168bcc8793
Bas Pennings <baspster@gmail.com>
2020-03-14 10:56:44 +0100

    Add compare methods to VM

--- src/vm.nim
+++ src/vm.nim
@@ -1,4 +1,4 @@
-import lists
+import hashes, lists
 
 # const maxSetSize = 32
 
@@ -92,22 +92,21 @@ method `==`*(a, b: List): bool =
       return false
     x = x.next
     y = y.next
-  true
 
 when isMainModule:
-  var a = newList(@[
+  var a = cast[Value](newList(@[
     cast[Value](newInt(1)),
-    cast[Value](newInt(2))])
-  var b = newList(@[
+    cast[Value](newInt(2))]))
+  var b = cast[Value](newList(@[
     cast[Value](newInt(1)),
-    cast[Value](newInt(2))])
-  var c = newList(@[
+    cast[Value](newInt(2))]))
+  var c = cast[Value](newList(@[
     cast[Value](newInt(1)),
     cast[Value](newInt(2)),
-    cast[Value](newInt(3))])
-  var d = newList(@[
+    cast[Value](newInt(3))]))
+  var d = cast[Value](newList(@[
     cast[Value](newInt(2)),
-    cast[Value](newInt(1))])
+    cast[Value](newInt(1))]))
   assert a == b
   assert b == a
   assert a != c
@@ -117,3 +116,93 @@ when isMainModule:
   assert d == d
   assert d != b
   assert b != d
+
+template literalHash(t: untyped) =
+  method hash*(a: t): Hash = hash(a.val)
+
+method hash*(a: Value): Hash {.base.} =
+  raiseRuntimeError("badarg for `hash`")
+
+literalHash(Bool)
+literalHash(Char)
+literalHash(Int)
+literalHash(Float)
+literalHash(Set)
+literalHash(String)
+literalHash(Ident)
+
+when isMainModule:
+  a = cast[Value](newBool(true))
+  b = cast[Value](newBool(false))
+  assert hash(a) == hash(true)
+  assert hash(b) == hash(false)
+  a = cast[Value](newIdent("foo"))
+  b = cast[Value](newIdent("bar"))
+  assert hash(a) == hash("foo")
+  assert hash(b) == hash("bar")
+
+method cmp*(a: Value, b: Value): Int {.base.} =
+  raiseRuntimeError("TILT cmp")
+method cmp*(a: Int, b: Int): Int =
+  newInt(cmp(a.val, b.val))
+method cmp*(a: Int, b: Float): Int =
+  newInt(cmp(a.val.float, b.val))
+method cmp*(a: Float, b: Int): Int =
+  newInt(cmp(a.val, b.val.float))
+method cmp*(a: Bool, b: Bool): Int =
+  newInt(cmp(a.val, b.val))
+method cmp*(a: Bool, b: Int): Int =
+  newInt(cmp(ord(a.val), b.val))
+method cmp*(a: Int, b: Bool): Int =
+  newInt(cmp(a.val, ord(b.val)))
+method cmp*(a: Bool, b: Char): Int =
+  newInt(cmp(ord(a.val), ord(b.val)))
+method cmp*(a: Char, b: Bool): Int =
+  newInt(cmp(ord(a.val), ord(b.val)))
+method cmp*(a: Char, b: Char): Int =
+  newInt(cmp(a.val, b.val))
+method cmp*(a: Char, b: Int): Int =
+  newInt(cmp(ord(a.val), b.val))
+method cmp*(a: Int, b: Char): Int =
+  newInt(cmp(a.val, ord(b.val)))
+method cmp*(a: Set, b: Set): Int =
+  newInt(cmp(a.val, b.val))
+method cmp*(a: String, b: String): Int =
+  newInt(cmp(a.val, b.val))
+method cmp*(a, b: List): Int =
+  var x = a.val.head
+  var y = b.val.head
+  while x != nil or y != nil:
+    if x == nil:
+      return newInt(-1)
+    if y == nil:
+      return newInt(1)
+    let z = cmp(x.value, y.value)
+    if z.val != 0:
+      return z
+    x = x.next
+    y = y.next
+  return newInt(0)
+
+when isMainModule:
+  a = cast[Value](newList(@[
+    cast[Value](newInt(1)),
+    cast[Value](newInt(2))]))
+  b = cast[Value](newList(@[
+    cast[Value](newInt(1)),
+    cast[Value](newInt(2))]))
+  c = cast[Value](newList(@[
+    cast[Value](newInt(1)),
+    cast[Value](newInt(2)),
+    cast[Value](newInt(3))]))
+  d = cast[Value](newList(@[
+    cast[Value](newInt(2)),
+    cast[Value](newInt(1))]))
+  assert cmp(a, b) == newInt(0)
+  assert cmp(b, a) == newInt(0)
+  assert cmp(a, c) == newInt(-1)
+  assert cmp(c, a) == newInt(1)
+  assert cmp(a, a) == newInt(0)
+  assert cmp(a, d) == newInt(-1)
+  assert cmp(d, a) == newInt(1)
+  
